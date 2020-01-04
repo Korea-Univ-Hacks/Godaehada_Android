df21b4037a5fec02193f6fdc3779c10126fbc651
KimYoonJin <95yjkim95@gmail.com>
2020-01-05 07:04:55 +0900

    navigation drawer

--- app/src/main/java/com/korea/hacks/view/main/MainActivity.kt
+++ app/src/main/java/com/korea/hacks/view/main/MainActivity.kt
@@ -2,10 +2,15 @@ package com.korea.hacks.view.main
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.drawable.Drawable
+import android.view.MenuItem
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.GridLayoutManager
+import com.google.android.material.navigation.NavigationView
 import com.korea.hacks.CallEvent
 import com.korea.hacks.R
 import com.korea.hacks.base.BaseActivity
@@ -13,10 +18,15 @@ import com.korea.hacks.databinding.ActivityMainBinding
 import com.korea.hacks.view.CreateRecyclerViewAdapter
 import com.korea.hacks.view.Test
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.navigation_header.view.*
 import kotlinx.android.synthetic.main.toolbar.view.*
 
-class MainActivity : BaseActivity<ActivityMainBinding>(){
+class MainActivity : BaseActivity<ActivityMainBinding>(),
+    NavigationView.OnNavigationItemSelectedListener {
     //class MainActivity : BaseActivity<ActivityMainBinding>(), CallEvent{
+
+    private val FINISH_INTERVAL_TIME: Long = 2000
+    private var backPressedTime: Long = 0
 
     override val layoutRes = R.layout.activity_main
 
@@ -41,17 +51,9 @@ class MainActivity : BaseActivity<ActivityMainBinding>(){
         creatorList.add(Test(name, tag))
     }
 
-    override fun onDataBinding() {
-        super.onDataBinding()
-    }
-
-    override fun setupView() {
-
+    override fun setupView(){
+        onCloseKeyboard()
         txt1.isSelected = true
-
-        binding.mainToolbar.menu_btn.setOnClickListener {
-            //TODO 햄버거 버튼 누르면 열리기
-        }
 
         test()
 
@@ -64,8 +66,21 @@ class MainActivity : BaseActivity<ActivityMainBinding>(){
             layoutManager = gridLayoutManager
             setHasFixedSize(true)
         }
-
         createRecyclerViewAdapter.setItems(creatorList)
+
+        /**
+         * 메뉴버튼
+         */
+        binding.mainToolbar.menu_btn.setOnClickListener {
+            binding.drawerLayout1.openDrawer(GravityCompat.START)
+        }
+        binding.navView.setNavigationItemSelectedListener(this)
+
+
+
+    //TODO 메뉴 이름, 사진, 입력..
+        //binding.navView.photo_img.setImageResource(R.drawable.group_5)
+        //binding.navView.name_txt.text = "김윤진"
 
         /**
          * 검색버튼 실행시키는 이벤트
@@ -117,6 +132,9 @@ class MainActivity : BaseActivity<ActivityMainBinding>(){
         }
     }
 
+
+
+
     /**
      * 검색 버튼 또는 카테고리 선택시 실행!!
      */
@@ -152,7 +170,6 @@ class MainActivity : BaseActivity<ActivityMainBinding>(){
      * 데일리 다이어리 리스트에서 특정 다이어리를 클릭하면 상세보기 화면으로 이동
      */
 
-
     private fun buttonInit() {
         binding.txt1.isSelected = false
         binding.txt2.isSelected = false
@@ -162,10 +179,10 @@ class MainActivity : BaseActivity<ActivityMainBinding>(){
     }
 
     private fun onCloseKeyboard() {
-        this.currentFocus?.let { view ->
+        this.currentFocus.let { view ->
             val inputMethodManager =
                 getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            inputMethodManager.hideSoftInputFromWindow(view?.windowToken, 0)
         }
     }
 
@@ -177,6 +194,43 @@ class MainActivity : BaseActivity<ActivityMainBinding>(){
 ////
 ////        startActivity(intent)
 //    }
+
+
+
+    override fun onBackPressed() {
+        val tempTime = System.currentTimeMillis()
+        val intervalTime = tempTime - backPressedTime
+
+        if (binding.drawerLayout1.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayout1.closeDrawers()
+        } else {
+            if (!(0 > intervalTime || FINISH_INTERVAL_TIME < intervalTime)) {
+                super.onBackPressed()
+            } else {
+                backPressedTime = tempTime
+                Toast.makeText(this, "뒤로 버튼을 한번 더 누르면 종료됩니다", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+            R.id.menuitem1->{
+                //TODO 나의 포트폴리오 화면 띄우기!!
+//          val intent = Intent(this, DetailActivity::class.java)
+////        intent.putExtra("diary", diary)
+////        startActivity(intent)
+               Toast.makeText(this, "나의 포트폴리오 화면 ", Toast.LENGTH_SHORT).show()
+            }
+            R.id.menuitem2->Toast.makeText(this, "공지사항", Toast.LENGTH_SHORT).show()
+
+            R.id.menuitem3->Toast.makeText(this, "도움말", Toast.LENGTH_SHORT).show()
+
+        }
+        binding.drawerLayout1.closeDrawers()   //close drawer after function do
+        return false
+    }
 
 
 }
