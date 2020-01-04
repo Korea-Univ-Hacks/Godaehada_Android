adc3625ee50cb35127b45d691fe674536db8382a
KimYoonJin <95yjkim95@gmail.com>
2020-01-04 19:56:21 +0900

    stop creating viewpager..

--- app/src/main/java/com/korea/hacks/view/main/MainActivity.kt
+++ app/src/main/java/com/korea/hacks/view/main/MainActivity.kt
@@ -1,18 +1,26 @@
 package com.korea.hacks.view.main
 
+import androidx.databinding.DataBindingUtil
 import com.korea.hacks.R
 import com.korea.hacks.base.BaseActivity
 import com.korea.hacks.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.toolbar.view.*
 
 class MainActivity: BaseActivity<ActivityMainBinding>() {
+
     override val layoutRes = R.layout.activity_main
 
     override fun onDataBinding() {
-        // TODO
         super.onDataBinding()
     }
 
     override fun setupView() {
-        // TODO
+        binding.mainToolbar.menu_btn.setOnClickListener{
+        //TODO 햄버거 버튼 누르면 열리기
+
+        }
+
+
     }
 }
